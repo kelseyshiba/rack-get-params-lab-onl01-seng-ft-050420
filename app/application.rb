e32d47c0b66bd2c10d6a54fa92330cf901e6b79a
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-06-05 20:21:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -43,6 +43,7 @@ class Application
   def handle_item_search(search_term)
     if @@items.include?(search_term)
       return "added #{search_term}"
+      @@cart << search_term
     else
       return "We don't have that item"
     end
